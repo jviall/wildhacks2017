ba1b911867aeb00d8cf5dcb799704b2b58288d52
Ellery Kreloff <kreloff@dhcp-10-105-166-139.wireless.northwestern.private>
2017-11-04 14:06:53 -0500

    i hate git

---
+++ public/javascripts/listen.js
@@ -0,0 +1,31 @@
+
+//recognition.continuous = true;
+var shouldRecord = true;
+
+function startRecord(callback){
+    var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'en-US';
+    recognition.interimResults = false;
+    recognition.maxAlternatives = 5;
+    recognition.start();
+    recognition.onresult = function (event) {
+        recognition.stop();
+        console.log('You said: ', event.results[0][0].transcript);
+        callback(event.results[0][0].transcript);
+        if(shouldRecord){
+            startRecord(function () {
+
+            });
+        }
+
+        //recognition.start();
+    };
+    callback();
+}
+function stopRecording(recognition){
+    shouldRecord = false;
+}
+
+
+
+
