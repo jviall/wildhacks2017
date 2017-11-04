8efd8a39551bc7a282f4369fdc4ce73ee3f5db75
Yazeed Sabri <sabri.yazeed@gmail.com>
2017-11-03 23:44:05 -0500

    Added some dep init into app

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
+var compression = require('compression');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
+var helmet = require('helmet');
 
 var app = express();
+
+//setup helmet and compress
+app.use(helmet());
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +25,44 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+var index = require('./routes/index');
+
+//var users = require('./routes/users');
+// The application will create a renderer using WebGL, if possible,
+// with a fallback to a canvas render. It will also setup the ticker
+// and the root stage PIXI.Container.
+var app = new PIXI.Application();
+
+// The application will create a canvas element for you that you
+// can then insert into the DOM.
+document.body.appendChild(app.view);
+
+// load the texture we need
+PIXI.loader.add('bunny', 'bunny.png').load(function(loader, resources) {
+
+    // This creates a texture from a 'bunny.png' image.
+    var bunny = new PIXI.Sprite(resources.bunny.texture);
+
+    // Setup the position of the bunny
+    bunny.x = app.renderer.width / 2;
+    bunny.y = app.renderer.height / 2;
+
+    // Rotate around the center
+    bunny.anchor.x = 0.5;
+    bunny.anchor.y = 0.5;
+
+    // Add the bunny to the scene we are building.
+    app.stage.addChild(bunny);
+
+    // Listen for frame updates
+    app.ticker.add(function() {
+         // each frame we spin the bunny around a bit
+        bunny.rotation += 0.01;
+    });
+});
 app.use('/', index);
-app.use('/users', users);
+//app.use('/users', users);
 
 app.listen(8080, function() {
 	console.log('listening on port 8080');
