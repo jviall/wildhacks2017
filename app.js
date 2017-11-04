213346c523828c46f34ab80591941bf78c6104e8
Yazeed Sabri <sabri.yazeed@gmail.com>
2017-11-04 00:42:32 -0500

    Pixi works now # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   app.js #	new file:   public/javascripts/pixi.min.js #	modified:   views/index.hbs #

--- app.js
+++ app.js
@@ -6,8 +6,38 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var helmet = require('helmet');
+//var PIXI = require('pixi');
 
 var app = express();
+// var px = new PIXI.Application();
+
+// // The application will create a canvas element for you that you
+// // can then insert into the DOM.
+// document.body.appendChild(px.view);
+
+// // load the texture we need
+// PIXI.loader.add('bunny', 'bunny.png').load(function(loader, resources) {
+
+//     // This creates a texture from a 'bunny.png' image.
+//     var bunny = new PIXI.Sprite(resources.bunny.texture);
+
+//     // Setup the position of the bunny
+//     bunny.x = app.renderer.width / 2;
+//     bunny.y = app.renderer.height / 2;
+
+//     // Rotate around the center
+//     bunny.anchor.x = 0.5;
+//     bunny.anchor.y = 0.5;
+
+//     // Add the bunny to the scene we are building.
+//     app.stage.addChild(bunny);
+
+//     // Listen for frame updates
+//     app.ticker.add(function() {
+//          // each frame we spin the bunny around a bit
+//         bunny.rotation += 0.01;
+//     });
+// });
 
 //setup helmet and compress
 app.use(helmet());
@@ -27,40 +57,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 var index = require('./routes/index');
-
 //var users = require('./routes/users');
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container.
-var app = new PIXI.Application();
 
-// The application will create a canvas element for you that you
-// can then insert into the DOM.
-document.body.appendChild(app.view);
-
-// load the texture we need
-PIXI.loader.add('bunny', 'bunny.png').load(function(loader, resources) {
-
-    // This creates a texture from a 'bunny.png' image.
-    var bunny = new PIXI.Sprite(resources.bunny.texture);
-
-    // Setup the position of the bunny
-    bunny.x = app.renderer.width / 2;
-    bunny.y = app.renderer.height / 2;
-
-    // Rotate around the center
-    bunny.anchor.x = 0.5;
-    bunny.anchor.y = 0.5;
-
-    // Add the bunny to the scene we are building.
-    app.stage.addChild(bunny);
-
-    // Listen for frame updates
-    app.ticker.add(function() {
-         // each frame we spin the bunny around a bit
-        bunny.rotation += 0.01;
-    });
-});
 app.use('/', index);
 //app.use('/users', users);
 
