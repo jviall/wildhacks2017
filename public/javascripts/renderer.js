164bb1e77b9881cf7693552897b6b870d9c1070d
Yazeed Sabri <sabri.yazeed@gmail.com>
2017-11-05 06:41:25 -0600

    Fixed the problem with the push, I am also adding the fairy # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   public/javascripts/nlp.js #	modified:   public/javascripts/renderer.js #	modified:   public/stylesheets/style.css #	modified:   views/index.hbs #

--- public/javascripts/renderer.js
+++ public/javascripts/renderer.js
@@ -1,7 +1,12 @@
+var forest = false, cave = false, bpotion = false, box = false, 
+    fairy = false;
 function backgroundToggle(fileName) {
     var element = document.getElementById("mainCanvas");
     if(fileName == "forest_backdrop.jpg"){
-        element.classList.toggle("forest");
+        if(!forest){
+           element.classList.toggle("forest");
+           forest = true;
+        }
     }
     else if(fileName == "cavern_backdrop.jpg"){
         element.classList.toggle("cave");
@@ -11,6 +16,9 @@ function backgroundToggle(fileName) {
     }
     else if(fileName == "treasure_box.png"){
         document.getElementById("treasure_chest").classList.toggle("display");
+    } 
+    else if(fileName == "fairy.png"){
+        document.getElementById("fairy").classList.toggle("display");
     }
     else if(fileName == "skull.png"){
         document.getElementById("skull").classList.toggle("display");
