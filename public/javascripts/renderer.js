0a342d0f24bf855992885318c18d9d99b056e63c
jviall <jamesviall@gmail.com>
2017-11-04 21:50:11 -0500

    Added sprites file to store info on object images.

--- public/javascripts/renderer.js
+++ public/javascripts/renderer.js
@@ -1,3 +1,6 @@
+
+var addToScene = undefined;
+
 window.onload = function(){
     "use strict";
     var mainCanvas = document.getElementById('mainCanvas');
@@ -10,10 +13,43 @@ window.onload = function(){
     var renderer = PIXI.autoDetectRenderer(canvasW, canvasH);
 
     var stage = new PIXI.Container();
-
     // Layer Containers
 
+    addToScene = function(imgObject){
+        var sprite;
+        if(!imgObject || imgObject.fileName == null || imgObject.fileType == null) {
+            console.log("imgObject was null or empty")
+            return null;
+        }
+        try {
+            var fileName = imgObject.fileName;
+            var objectType = imgObject.fileType;
+            sprite = new PIXI.Sprite.fromImage('../images/'+ objectType +'s/' + fileName);
+            sprite.anchor.set(0.5, 0.5);
 
+            //If this object has a scale property, set it
+            if(imgObject.scale)
+                sprite.scale(imgObject.scale);
+
+            //Add it to the proper container
+            if(objectType == 'backdrop')
+                stage.zIndex = 0;
+
+            else if (objectType == 'object') {
+                if (imgObject.Sky)
+                    foregroundSky.addChild(sprite);
+                else
+                    foreground.addChild(sprite);
+
+                stage.addChild(sprite);
+            }
+            else
+                throw error "Object \'" + fileName + "\' does not have an object type, YAZEED.";
+        } catch(e){
+            console.log("addToScene(imgObject : string) : something wrong with imgObject (probably). Value: " + imgObject + " Error Message: " + e);
+            return null;
+        }
+    }
 
 
 
---
+++ public/javascripts/sprites.js
@@ -0,0 +1,21 @@
+var sprites = {
+    backdrop : {
+        forest_backdrop : {
+            name : 'forest_backdrop',
+            fileName : 'forest_backdrop.jpg',
+            fileType : 'backdrop',
+            x : 1920,
+            y : 1200
+        },
+        cavern_bacykdrop : {
+            name : 'cavern_backdrop',
+            fileName : 'cavern_backdrop.jpg',
+            fileType : 'backdrop',
+            x : 1191,
+            y : 670
+        }
+    },
+    object : {
+
+    }
+}
