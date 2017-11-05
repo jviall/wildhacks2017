9a9fa8bc3d2dbdee4f5395e730e8b5201082f3a6
Yazeed Sabri <sabri.yazeed@gmail.com>
2017-11-04 20:23:44 -0500

    the nlp parses verbs, nouns, adjs of sentences, aka strings # Please enter the commit message for your changes. Lines starting # with '#' will be kept; you may remove them yourself if you want to. # An empty message aborts the commit. # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #	modified:   public/javascripts/nlp.js #	modified:   views/index.hbs #

--- public/javascripts/nlp.js
+++ public/javascripts/nlp.js
@@ -1,5 +1,18 @@
 console.log('wudd aap');
 
-function nlp(str){
+function myNlp(str, callback){
 	console.log(str);
+	var nouns = nlp(str).nouns().out('array');
+	console.log('nouns: ' + nouns);
+	var verbs = nlp(str).verbs().out('array');
+	console.log('verbs: ' + verbs);
+	var adj = nlp(str).adjectives().out('array');
+	console.log('adj: ' + adj);
+	//console.log(parse.out('text'));
+	// parse = nlp(str);
+	// parse.verbs().data();
+	// console.log(parse);
+	// parse.adjectives().data();
+	// console.log(parse);
+	callback();
 }
