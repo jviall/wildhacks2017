2a3a5ff9a632d6c94c08758ecc0a1a9374f42bb3
Yazeed Sabri <sabri.yazeed@gmail.com>
2017-11-05 01:10:14 -0500

    solved a stash merge conflict

--- public/javascripts/nlp.js
+++ public/javascripts/nlp.js
@@ -1,18 +1,45 @@
 console.log('wudd aap');
+var text = ['forset', 'treasure chest', 'sword', 'mace', 'potion', 'golden mask', 'skull', 'fairy'];
+
+function parseNouns(str, callback){
+	var nouns = nlp(str).nouns().out('array');
+	console.log('nouns: ' + nouns);
+	callback(nouns);
+}
+
 
 function myNlp(str, callback){
 	//console.log(str);
-	var nouns = nlp(str).nouns().out('array');
-	console.log('nouns: ' + nouns);
+	parseNouns(function(nouns) {
+		for(var i = 0; i<nouns.length; i++){
+			if(text.indexOf(nouns[i]) > -1){
+				switch(nouns[i]){
+					case 'forset':
+						break;
+					case 'treasure chest':
+						break;
+					case 'sword':
+						break;
+					case 'mace':
+						break;
+					case 'potion':
+						break;
+					case 'golden mask':
+						break;
+					case 'skull':
+						break;
+					case 'fairy':
+						break;
+				}
+			}
+		}
+	});
+
+	
 	var verbs = nlp(str).verbs().out('array');
-	console.log('verbs: ' + verbs);
+	//console.log('verbs: ' + verbs);
 	var adj = nlp(str).adjectives().out('array');
-	console.log('adj: ' + adj);
-	//console.log(parse.out('text'));
-	// parse = nlp(str);
-	// parse.verbs().data();
-	// console.log(parse);
-	// parse.adjectives().data();
-	// console.log(parse);
+	//console.log('adj: ' + adj);
+	
 	callback();
 }
